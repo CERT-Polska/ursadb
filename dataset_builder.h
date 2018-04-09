a2a8f1257978ebdba0f60f24cc766e83bead5298
msm <msm2e4d534d@gmail.com>
2018-04-09 23:11:10 +0100

    Fix text and gitignore vs code

--- dataset_builder.h
+++ dataset_builder.h
@@ -8,12 +8,12 @@
 
 
 class DatasetBuilder {
-    std::vector <std::string> fids;
+    std::vector<std::string> fids;
     std::array<std::vector<FileId>, 16777216> index;
 
 public:
     FileId register_fname(const std::string &fname);
     void add_trigram(const FileId &fid, const TriGram &val);
-    std::vector<uint8_t> compress_run(const std::vector <FileId> &files);
+    std::vector<uint8_t> compress_run(const std::vector<FileId> &files);
     void save(const std::string &fname);
 };
