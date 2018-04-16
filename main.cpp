3d7459bc105dcc4409f3f853d937702f84345eb9
Michał Leszczyński <ml@icedev.pl>
2018-04-16 12:41:40 +0200

    fix command line interface

--- main.cpp
+++ main.cpp
@@ -13,10 +13,11 @@
 
 
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
+    if (argc < 3) {
         printf("Usage:\n");
-        printf("%s index [database] [file]\n", argv[0]);
-        printf("%s query [database] [query]\n", argv[0]);
+        printf("    %s index [database] [file]\n", argv[0]);
+        printf("    %s query [database] [query]\n", argv[0]);
+        printf("    %s compact [database]\n", argv[0]);
         fflush(stdout);
         return 1;
     }
@@ -25,12 +26,23 @@ int main(int argc, char *argv[]) {
     if (argv[1] == std::string("index")) {
         Database db(dbpath);
         DatasetBuilder builder;
+
+        if (argc <= 3) {
+            std::cout << "nothing to index" << std::endl;
+            return 1;
+        }
+
         for (int i = 3; i < argc; i++) {
             builder.index(argv[i]);
         }
         db.add_dataset(builder);
         db.save();
     } else if (argv[1] == std::string("query")) {
+        if (argc != 4) {
+            std::cout << "too few or too many arguments" << std::endl;
+            return 1;
+        }
+
         Query test = parse_query(argv[3]);
         std::cout << test << std::endl;
         Database db(argv[2]);
@@ -40,6 +52,11 @@ int main(int argc, char *argv[]) {
             std::cout << s << std::endl;
         }
     } else if (argv[1] == std::string("compact")) {
+        if (argc != 3) {
+            std::cout << "too few or too many arguments" << std::endl;
+            return 1;
+        }
+
         Database db(argv[2]);
         db.compact();
     }
