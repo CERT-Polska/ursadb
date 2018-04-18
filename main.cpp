bd68dc4672bf3b00531608c62e0b462e8cd9b0e1
Michał Leszczyński <michal.leszczynski@cert.pl>
2018-04-18 17:11:31 +0200

    implemented zeromq server

--- main.cpp
+++ main.cpp
@@ -14,20 +14,28 @@
 #include "OnDiskDataset.h"
 #include "QueryParser.h"
 
-void execute_command(const Command &cmd, Database *db) {
-        if (std::holds_alternative<SelectCommand>(cmd)) {
-            const Query &select = std::get<SelectCommand>(cmd).get_query();
-            std::cout << select << std::endl;
-            std::vector<std::string> out;
-            db->execute(select, out);
-            for (std::string &s : out) {
-                std::cout << s << std::endl;
-            }
-        } else if (std::holds_alternative<IndexCommand>(cmd)) {
-            const std::string &path = std::get<IndexCommand>(cmd).get_path();
-            std::vector<IndexType> types = {IndexType::GRAM3, IndexType::TEXT4};
-            db->index_path(types, path);
+std::string execute_command(const Command &cmd, Database *db) {
+    std::stringstream ss;
+
+    if (std::holds_alternative<SelectCommand>(cmd)) {
+        const Query &select = std::get<SelectCommand>(cmd).get_query();
+        std::cout << select << std::endl;
+        std::vector<std::string> out;
+        db->execute(select, out);
+        ss << "OK" << std::endl;
+        for (std::string &s : out) {
+            ss << s << std::endl;
         }
+    } else if (std::holds_alternative<IndexCommand>(cmd)) {
+        const std::string &path = std::get<IndexCommand>(cmd).get_path();
+        std::vector<IndexType> types = {IndexType::GRAM3, IndexType::TEXT4};
+        db->index_path(types, path);
+        ss << "OK" << std::endl;
+    } else {
+        throw std::runtime_error("Invalid command type.");
+    }
+
+    return ss.str();
 }
 
 int main(int argc, char *argv[]) {
@@ -49,7 +57,8 @@ int main(int argc, char *argv[]) {
         }
         Database db(argv[2]);
         Command cmd = parse_command(argv[3]);
-        execute_command(cmd, &db);
+        std::string res = execute_command(cmd, &db);
+        std::cout << res << std::endl;
     } else if (argv[1] == std::string("compact")) {
         if (argc != 3) {
             std::cout << "too few or too many arguments" << std::endl;
@@ -59,20 +68,32 @@ int main(int argc, char *argv[]) {
         Database db(argv[2]);
         db.compact();
     } else if (argv[1] == std::string("server")) {
+        Database db(argv[2]);
+
         zmq::context_t context(1);
         zmq::socket_t socket(context, ZMQ_REP);
-        socket.bind("tcp://*:5555");
+        socket.bind("tcp://*:9281");
 
         while (true) {
-            // TODO implement actual server
             zmq::message_t request;
 
             socket.recv(&request);
-            std::cout << "Received Hello" << std::endl;
+            std::string cmd_str = std::string(static_cast<char*>(request.data()), request.size());
+            std::cout << "Received request " << cmd_str << std::endl;
 
-            zmq::message_t reply(5);
-            memcpy(reply.data(), "World", 5);
-            socket.send(reply);
+            try {
+                Command cmd = parse_command(cmd_str);
+                std::string s = execute_command(cmd, &db);
+                zmq::message_t reply(s.data(), s.size());
+                socket.send(reply);
+            } catch (std::runtime_error &e) {
+                std::cout << "Command failed: " << e.what() << std::endl;
+                std::stringstream ss;
+                ss << "ERR " << e.what() << std::endl;
+                std::string s = ss.str();
+                zmq::message_t reply(s.data(), s.size());
+                socket.send(reply);
+            }
         }
     }
     return 0;
