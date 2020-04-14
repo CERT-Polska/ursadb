bb9585d849723c31cf0e292b84b3d62b61ff58de
root <ml@icedev.pl>
2020-04-13 21:32:43 -0400

    add command line parsing to client, minor fixes

--- src/Client.h
+++ src/Client.h
@@ -1,24 +1,29 @@
 #pragma once
 
+#include <atomic>
+
 #include <zmq.hpp>
 
 class UrsaClient {
 private:
-    bool command_active = false;
-    bool terminated = false;
+    std::atomic<bool> command_active = false;
+    std::atomic<bool> terminated = false;
+    std::atomic<uint64_t> wait_time = 0;
 
     std::string server_addr;
     std::string db_command;
+    bool quiet_mode;
 
     std::string server_version;
     std::string connection_id;
 
+    bool wait_sec();
     void status_worker();
     void init_conn(zmq::socket_t& socket);
     void recv_res(zmq::socket_t& socket);
 
 public:
-    UrsaClient(std::string server_addr, std::string db_command);
+    UrsaClient(std::string server_addr, std::string db_command, bool silent_mode);
     int start();
 };
 
