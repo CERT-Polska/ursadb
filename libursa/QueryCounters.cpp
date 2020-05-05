0df8b75d2ca789368d9a598520c953b96ed85352
msm <msm@tailcall.net>
2020-05-05 10:22:41 +0200

    Move querycounters out of queryresults file

---
+++ libursa/QueryCounters.cpp
@@ -0,0 +1,29 @@
+#include "QueryCounters.h"
+
+#include <algorithm>
+
+void QueryCounter::add(const QueryCounter &other) {
+    count_ += other.count_;
+    duration_ += other.duration_;
+}
+
+void QueryCounters::add(const QueryCounters &other) {
+    ors_.add(other.ors_);
+    ands_.add(other.ands_);
+    reads_.add(other.reads_);
+    minofs_.add(other.minofs_);
+}
+
+std::unordered_map<std::string, QueryCounter> QueryCounters::counters() const {
+    std::unordered_map<std::string, QueryCounter> result;
+    result["or"] = ors_;
+    result["and"] = ands_;
+    result["read"] = reads_;
+    result["minof"] = minofs_;
+    return result;
+}
+
+QueryOperation::~QueryOperation() {
+    auto duration = std::chrono::steady_clock::now() - start_;
+    parent->add(QueryCounter(1, duration));
+}
